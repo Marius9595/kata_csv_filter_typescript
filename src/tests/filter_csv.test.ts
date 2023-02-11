564cb77b2a18c53eab55c7ccabf0bbb3a6f1d36b
Mario Pinto <mario@leanmind.es>
2023-02-11 19:29:15 +0000

    🔴 filter csv should filter an invoice whose net was not correctly calculated 🚧 FIX TYPO a i.. > an i..

--- src/tests/filter_csv.test.ts
+++ src/tests/filter_csv.test.ts
@@ -14,8 +14,6 @@ Headers: Num_invoice, Date, Gross, Net ,IVA, IGIC, Concept, CIF_client, NIF_clie
     - ["<Num_invoice>,<Date>,<Gross>,<Net>,<IVA>,<IGIC>,<Concept>,B76430134,F74433134"] -> []
 
     Net = Gross * Taxes(IVA or IGIC)
-    - ["<Num_invoice>,<Date>,100,119,19,,<Concept>,<NIF_client>,<CIF_client>"] -> ["<Num_invoice>,<Date>,100,119,19,,<Concept>,<NIF_client>,<CIF_client>"]
-    - ["<Num_invoice>,<Date>,100,119,,19,<Concept>,<NIF_client>,<CIF_client>"] -> ["<Num_invoice>,<Date>,100,119,,19,<Concept>,<NIF_client>,<CIF_client>"]
     - ["<Num_invoice>,<Date>,100,120,19,,<Concept>,<NIF_client>,<CIF_client>"] -> []
 
     If there are more than one facture with same reference (number of facture), these are invalid
@@ -41,7 +39,7 @@ describe('filter csv should', () => {
 		expect(csv_filtered).toBe(csv);
 	});
 
-	it('filter a invoice with IVA and IGIC declared because are mutually exclusive', () => {
+	it('filter an invoice with IVA and IGIC declared because are mutually exclusive', () => {
 		const invoice = '1,02/05/2019,1008,810,19,19,ACERLaptop,B76430134,';
 		const csv = [header, invoice];
 
@@ -50,8 +48,17 @@ describe('filter csv should', () => {
 		expect(csv_filtered).toStrictEqual([]);
 	});
 
-	it('filter a invoice with NIF_client and CIF_client declared because are mutually exclusive', () => {
+	it('filter an invoice with NIF_client and CIF_client declared because are mutually exclusive', () => {
 		const invoice = '1,02/05/2019,1008,810,19,,ACERLaptop,B76430134,F74433134';
+		const csv = [header, invoice];
+
+		const csv_filtered = new FilterCSV().filter(csv);
+
+		expect(csv_filtered).toStrictEqual([]);
+	});
+
+	it('filter an invoice whose net was not correctly calculated', () => {
+		const invoice = '1,02/05/2019,100,121,20,,ACERLaptop,B76430134,';
 		const csv = [header, invoice];
 
 		const csv_filtered = new FilterCSV().filter(csv);
