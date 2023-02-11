41d658857190231cd52798b26b13b3a50f838561
Mario Pinto <mario@leanmind.es>
2023-02-11 19:22:18 +0000

    🔵 Extracting explaining variable for array elements and conditionals

--- src/core/csv_filter.ts
+++ src/core/csv_filter.ts
@@ -2,10 +2,18 @@ export class FilterCSV {
 	filter(csv: string[]): string[] {
 		const invoices = csv.slice(1);
 		const invoice = invoices[0].split(',');
-		if (invoice[4] !== '' && invoice[5] !== '') {
+		const iva = invoice[4];
+		const igic = invoice[5];
+		const cif_client = invoice[7];
+		const nif_client = invoice[8];
+		const iva_is_declared = iva !== '';
+		const igic_was_declared = igic !== '';
+		if (iva_is_declared && igic_was_declared) {
 			return [];
 		}
-		if (invoice[7] !== '' && invoice[8] !== '') {
+		const cif_client_is_declared = cif_client !== '';
+		const nif_is_declared = nif_client !== '';
+		if (cif_client_is_declared && nif_is_declared) {
 			return [];
 		}
 		return csv;
